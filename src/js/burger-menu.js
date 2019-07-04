2e948e3ed7b250fbf04059ba2ac17a9e6e7dc299
Mohamed Adel <ov3rcontrol@hotmail.com>
2019-07-04 16:56:44 +0200

    Add Burger Menu

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,11 +1,3 @@
-$(document).ready(function() {
-
-  // Check for click events on the navbar burger icon
-  $(".burger").click(function() {
-
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-
-  });
+$(".menu-collapsed").click(function() {
+  $(this).toggleClass("menu-expanded");
 });
